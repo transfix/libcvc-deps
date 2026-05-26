1f00ebbf7aa6c2c3feaa4b720cafc72167db4c4c
Joe Rivera <j@jriv.us>
2026-05-26 18:37:41 -0500

    fix: also exclude boost asio/cobalt for wasm (depends on context)

--- recipes/boost/build-wasm.ps1
+++ recipes/boost/build-wasm.ps1
@@ -10,5 +10,5 @@ Invoke-CvcWasmCMakeBuild @(
     '-DBUILD_TESTING=OFF',
     '-DBOOST_INSTALL_LAYOUT=system',
     '-DCMAKE_CXX_FLAGS=-DBOOST_HAS_PTHREADS',
-    '-DBOOST_EXCLUDE_LIBRARIES=context;coroutine;fiber;stacktrace'
+    '-DBOOST_EXCLUDE_LIBRARIES=context;coroutine;fiber;stacktrace;asio;cobalt'
 )
